2247b409f05524992a7aa95cabbee98b5a3f2bdb
Piteira1406 <joaoduarte1406@gmail.com>
2025-10-30 19:22:54 +0000

    firebase connection

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,24 +1,23 @@
 // features/auth/screens/login_screen.dart
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:itasks/core/services/auth_service.dart';
 import 'package:itasks/features/auth/providers/login_provider.dart';
 import 'package:itasks/features/auth/widgets/login_form.dart';
-import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Vamos buscar os serviços globais que foram fornecidos no main.dart
+    // Vamos buscar o AuthService global que foi fornecido no main.dart
     final authService = context.read<AuthService>();
-    final authProvider = context.read<AuthProvider>();
 
     // O ChangeNotifierProvider cria o LoginProvider *apenas* para este ecrã.
     // Quando o ecrã for destruído, o LoginProvider também é.
     return ChangeNotifierProvider(
-      create: (_) =>
-          LoginProvider(authService: authService, authProvider: authProvider),
+      create: (_) => LoginProvider(authService: authService),
       child: Scaffold(
         // Pode usar a sua ScrollFrostAppBar aqui se quiser
         appBar: AppBar(
